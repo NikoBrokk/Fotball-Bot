0bcb67e47c4dc3f55d86be30c7b4897161b78422
NikoBrokk <nikolaibrokhaug@gmail.com>
2025-09-01 15:28:06 +0200

    Update cli.py

--- src/cli.py
+++ src/cli.py
@@ -1,17 +1,15 @@
-import typer #kommandolinjer
-from pathlib import Path #filstier
-from rich import print #visuell tekst
+import typer  # kommandolinjer
+from pathlib import Path  # filstier
+from rich import print  # visuell tekst (farger/format)
 
-app = typer.Typer() #lag en app for kommandolinjer
+app = typer.Typer()  # lag en app for kommandolinjer
 
-@app.command() 
-def parse_invoices(dir: Path, out: Path = Path("data/out/out.csv")): #teller PDFene i katalogen dir
-  files = list(dir.glob("*pdf")) 
-  print(f"[bold grenn]Fant {len(files)} PDF-filer i {dir}[/bold green]")
-
-if __name__ == "__main__": #en test, spesifikk for cli.py
-  app() #kjør CLI appen
-
+@app.command()
+def parse_invoices(dir: Path, out: Path = Path("data/out/out.csv")):
+    """Teller PDFene i katalogen dir"""  
+    files = list(dir.glob("*.pdf"))  # finn alle filer som slutter på .pdf
+    print(f"[bold green]Fant {len(files)} PDF-filer i {dir}[/bold green]")  
+    # skriver ut antallet i grønt
 
 if __name__ == "__main__":  # sjekker om filen kjøres direkte
     app()  # starter CLI-app'en (typer håndterer argumenter/kommandoer)
